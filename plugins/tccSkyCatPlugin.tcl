b4e0d0ffc5ab13dbcb7b927cc06f3a20955c3cff
cjm <cjm@gemini.edu>
1999-11-20 22:32:18 +0000

    Display totally vignetted and partially vignetted areas due to guide probes and allow toggling between the two possible orientations
    git-svn-id: http://source.gemini.edu/software/tcc/trunk@8809 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- plugins/tccSkyCatPlugin.tcl
+++ plugins/tccSkyCatPlugin.tcl
@@ -35,6 +35,12 @@ proc SkyCat_plugin {this} {
    $w add_menuitem $gemmenu command "Set IPD" \
          "Set Instrument Principle Direction" \
          -command "TccSkyCat::pick_angle"
+   $w add_menuitem $gemmenu command "Toggle PWFS1 arm" \
+         "Select the other orientation for PWFS1 probe arm" \
+         -command "TccSkyCat::toggle_arm pwfs1"
+   $w add_menuitem $gemmenu command "Toggle PWFS2 arm" \
+         "Select the other orientation for PWFS2 probe arm" \
+         -command "TccSkyCat::toggle_arm pwfs2"
 
 # Set the name of the command called when a new image is loaded.
    set image [$w component image]
@@ -72,6 +78,30 @@ namespace eval TccSkyCat {
 # This procedure draws the Gemini field centred on the supplied position.
    proc draw_field {} {
       global tcc_interp canvas toplevel rtdimage
+      global togglePwfs1 togglePwfs2
+
+      set pwfs1Color     white       ;# Plot colour for pwfs1
+      set pwfs2Color     yellow      ;# Plot colour for pwfs2
+
+      if {![info exists togglePwfs1]} {
+        set togglePwfs1 subtract
+      } else {
+        if {$togglePwfs1 == "subtract"} {
+          set togglePwfs1 add
+        } else {
+          set togglePwfs1 subtract
+        }
+      }
+
+      if {![info exists togglePwfs2]} {
+        set togglePwfs2 subtract
+      } else {
+        if {$togglePwfs2 == "subtract"} {
+          set togglePwfs2 add
+        } else {
+          set togglePwfs2 subtract
+        }
+      }
 
 # Get the field centre in world coordinates.
       set centre [$rtdimage wcscenter -format 1]
@@ -80,51 +110,47 @@ namespace eval TccSkyCat {
       $rtdimage convert coords [lindex $centre 0] [lindex $centre 1] deg \
             xc yc canvas
 
-# Get the size of the rectangle that encloses the field radius (7 arcmin)
-# as canvas coords.
-      $rtdimage convert dist 0.117 0.117 deg xs ys canvas
+# Get the size of the rectangle that encloses the field radius (418")
+# of PWFS1 as canvas coords.
+      $rtdimage convert dist 0.1161 0.1161 deg xs ys canvas
 
 # Plot the field size.
       $canvas create oval [expr $xc - $xs] [expr $yc - $ys] \
-            [expr $xc + $xs] [expr $yc + $ys] -outline white -tags objects
+            [expr $xc + $xs] [expr $yc + $ys] -outline $pwfs1Color -tags objects
 
-# Radius (in pixels) of the WFS table - same as field size for now.
+# Derive conversion factor to go from mm in focal plane to pixels
+      set mm2Pixels [expr $xs * 1.611 / (0.1161 * 3600.0)]
+
+# Radius (in pixels) of the WFS table - same as field size of PWFS1 for now.
       set r $xs
+
+# Get the size of the rectangle that encloses the field radius (421")
+# of PWFS2 as canvas coords.
+      $rtdimage convert dist 0.1169 0.1169 deg xs ys canvas
+
+# Plot the field size.
+      $canvas create oval [expr $xc - $xs] [expr $yc - $ys] \
+            [expr $xc + $xs] [expr $yc + $ys] -outline $pwfs2Color -tags objects
 
 # Plot the wave front sensor positions
       foreach wfs {pwfs1_target pwfs2_target} {
+         if {$wfs == "pwfs1_target"} {
+           set pwfsColor $pwfs1Color
+           set toggle $togglePwfs1
+         } else {
+           set pwfsColor $pwfs2Color
+           set toggle $togglePwfs2
+         }
          set target [send $tcc_interp tccPreview get $wfs]
          if { $target != "" } {
             if { [catch {$rtdimage convert coords [lindex $target 0] \
                    [lindex $target 1] {wcs J2000} x y canvas} ] } {
             } else {
-               $canvas create polygon $x [expr $y - 6] [expr $x - 6] $y \
-                     $x [expr $y + 6] [expr $x + 6] $y -fill white -tags objects
    
 # Plot the two possible WFS arm positions.
+
+               plotWfs $canvas $wfs $x $y $xc $yc $mm2Pixels $toggle
    
-# WFS position relative to field centre.
-               set dx [expr $x - $xc]
-               set dy [expr $y - $yc]
-
-# Half the distance of the WFS from the field centre.
-               set s2 [expr sqrt ( $dx * $dx + $dy * $dy ) / 2]
-
-# Bearing of WFS.
-               set b [expr atan2 ( - $dy , $dx )]
-
-# Angle between WFS bearing and arm pivot.
-               set d [expr acos ( $s2 / $r )]
-
-# X/Y of pivot.
-               set x1 [expr $xc + $r * cos ( $b + $d )]
-               set y1 [expr $yc - $r * sin ( $b + $d )]
-               $canvas create line $x $y $x1 $y1 -fill white -tags objects
-
-# Similarly for the other possible position.
-               set x1 [expr $xc + $r * cos ( $b - $d )]
-               set y1 [expr $yc - $r * sin ( $b - $d )]
-               $canvas create line $x $y $x1 $y1 -fill white -tags objects
             }
          }
       }
@@ -249,4 +275,248 @@ namespace eval TccSkyCat {
       send $tcc_interp tcsconfigrotator select \"\"
       send $tcc_interp $control selectobj $name
    }
+
+# This procedure takes a  WFS object and draws its projected
+# shape in the focal plane of the image
+
+  proc plotWfs {canvas wfs x y xc yc mm2Pix toggle} {
+
+# Define constants that define geometry of the two PWFS
+
+      set m2FplaneSep 16539.326      ;# Distance from M2 to focal plane
+      set m2Radius    511.0          ;# Radius of M2 (mm)
+      set armLength      400.0       ;# Pivot to mirror centre (mm)
+      set pwfs1FplaneSep 1900.0      ;# Distance of pwfs1 from focal plane
+      set pwfs2FplaneSep 1615.0      ;# Distance of pwfs2 from focal plane
+      set pwfs1Mwidth    146.0       ;# Width of pickoff mirror (mm)
+      set pwfs1Mlength   146.0       ;# Length of pickoff mirror (mm)
+      set pwfs2Mwidth    122.0       ;# Width of pickoff mirror (mm)
+      set pwfs2Mlength   122.0       ;# Length of pickoff mirror (mm)
+      set pwfs1Awidth    196.0       ;# Width of probe arm (mm)
+      set pwfs2Awidth    196.0       ;# Width of probe arm (mm)
+      set pwfs1Adepth    196.0       ;# Depth of pwfs1 support (mm)
+      set pwfs2Adepth    196.0       ;# Depth of pwfs2 support (mm)
+      set pwfs1Color     white       ;# colour for pwfs1
+      set pwfs2Color     yellow      ;# colour for pwfs2
+
+# Set scale to convert mm in focal plane to mm at height of pwfs
+
+      set pwfs1Scale [expr ($m2FplaneSep - $pwfs1FplaneSep)/$m2FplaneSep]
+      set pwfs2Scale [expr ($m2FplaneSep - $pwfs2FplaneSep)/$m2FplaneSep]
+
+# Set up parameters for appropriate wfs
+
+      if {$wfs == "pwfs1_target"} {
+        set pwfsScale $pwfs1Scale
+        set pwfsColor $pwfs1Color
+        set pwfsFplaneSep $pwfs1FplaneSep
+        set pwfsMwidth $pwfs1Mwidth
+        set pwfsMlength $pwfs1Mlength
+        set pwfsAwidth $pwfs1Awidth
+      } else {
+        set pwfsScale $pwfs2Scale
+        set pwfsColor $pwfs2Color
+        set pwfsFplaneSep $pwfs2FplaneSep
+        set pwfsMwidth $pwfs2Mwidth
+        set pwfsMlength $pwfs2Mlength
+        set pwfsAwidth $pwfs2Awidth
+      }
+
+# WFS position relative to field centre with allowance for height of wfs
+# above focal plane.
+      set dx [expr ($x - $xc)*$pwfsScale]
+      set dy [expr ($y - $yc)*$pwfsScale]
+
+# Half the distance of the WFS from the field centre.
+      set s2 [expr sqrt ( $dx * $dx + $dy * $dy ) / 2]
+
+# Bearing of WFS.
+      set b [expr atan2 ( - $dy , $dx )]
+
+# Radius (in pixels) of the WFS table
+      set r [expr $armLength * $mm2Pix]
+
+# Angle between WFS bearing and arm pivot.
+      set d [expr acos ( $s2 / $r)]
+
+# X/Y of pivot.
+      set xp [expr $xc + $r * cos ( $b + $d )]
+      set yp [expr $yc - $r * sin ( $b + $d )]
+      $canvas create line $x $y $xp $yp -fill $pwfsColor -tags objects
+      if {$toggle == "add" } {
+        set xpSave $xp
+        set ypSave $yp
+      }
+
+# Similarly for the other possible position.
+      set xp [expr $xc + $r * cos ( $b - $d )]
+
+      set yp [expr $yc - $r * sin ( $b - $d )]
+      $canvas create line $x $y $xp $yp -fill $pwfsColor -tags objects
+      if {$toggle == "subtract" } {
+        set xpSave $xp
+        set ypSave $yp
+      }
+
+# Set offset for determining the completely vignetted/unvignetted coords
+# of a point
+      set dr [expr $m2Radius * $pwfsFplaneSep / $m2FplaneSep]
+
+# Coords of corners of object for fully vignetted case when it is aligned
+# along the x axis with the end of the object furthest from the pivot
+# point. 
+
+# Partially vignetted region
+
+      set x1 [expr ($pwfsMlength/2.0 + $dr) * $mm2Pix + $r]
+      set y1 [expr (-$pwfsMwidth/2.0 - $dr) * $mm2Pix ]
+
+      set x2 $x1
+      set y2 [expr -$y1]
+
+      set x3 [expr (-$pwfsMlength/2.0 + $dr) * $mm2Pix + $r]
+      set y3 $y2
+
+      set x4 $x3
+      set y4 $y1
+
+      set vigType partial 
+      plotShape $canvas $pwfsColor $pwfsScale $xc $yc $xpSave $ypSave $dx $dy \
+                $x1 $y1 $x2 $y2 $x3 $y3 $x4 $y4 $vigType
+
+      set x1 $x4
+      set y1 [expr (-$pwfsAwidth/2.0 - $dr) * $mm2Pix ]
+
+      set x2 $x1
+      set y2 [expr -$y1]
+
+      set x3 0.0
+      set y3 $y2
+
+      set x4 $x3
+      set y4 $y1
+
+      set vigType partial 
+      plotShape $canvas $pwfsColor $pwfsScale $xc $yc $xpSave $ypSave $dx $dy \
+                $x1 $y1 $x2 $y2 $x3 $y3 $x4 $y4 $vigType
+
+# Mirror
+
+      set x1 [expr ($pwfsMlength/2.0 - $dr) * $mm2Pix + $r]
+      set y1 [expr (-$pwfsMwidth/2.0 + $dr) * $mm2Pix ]
+
+      set x2 $x1
+      set y2 [expr -$y1]
+
+      set x3 [expr (-$pwfsMlength/2.0 + $dr) * $mm2Pix + $r]
+      set y3 $y2
+
+
+      set x4 $x3
+      set y4 $y1
+
+      set vigType clear 
+      plotShape $canvas $pwfsColor $pwfsScale $xc $yc $xpSave $ypSave $dx $dy \
+                $x1 $y1 $x2 $y2 $x3 $y3 $x4 $y4 $vigType
+
+# Arm
+
+      set x1 $x4
+      set y1 [expr (-$pwfsAwidth/2.0 + $dr) * $mm2Pix ]
+
+      set x2 $x1
+      set y2 [expr -$y1]
+
+      set x3 0.0
+      set y3 $y2
+
+      set x4 $x3
+      set y4 $y1
+
+      set vigType blocked 
+      plotShape $canvas $pwfsColor $pwfsScale $xc $yc $xpSave $ypSave $dx $dy \
+                $x1 $y1 $x2 $y2 $x3 $y3 $x4 $y4 $vigType
+
+  }
+
+# Routine to draw a polygon as with four corners in the correct orientation
+# and projection in the focal plane
+
+  proc plotShape {canvas pwfsColor pwfsScale xc yc xp yp dx dy
+                  x1 y1 x2 y2 x3 y3 x4 y4 vigType} {
+
+# Rotate each x,y pair to a frame aligned along the axis of the probe
+
+      set dxp [expr $dx + $xc - $xp]
+      set dyp [expr $dy + $yc - $yp]
+      set rot [expr atan2 ( -$dyp, $dxp)]
+
+      set cosrot [expr cos($rot)]
+      set sinrot [expr sin($rot)]
+
+      set x1p [expr ($x1 * $cosrot + $y1 * $sinrot) + $xp]
+      set y1p [expr (-$x1 * $sinrot + $y1 * $cosrot) + $yp]
+
+      set x2p [expr ($x2 * $cosrot + $y2 * $sinrot) + $xp]
+      set y2p [expr (-$x2 * $sinrot + $y2 * $cosrot) + $yp]
+
+      set x3p [expr ($x3 * $cosrot + $y3 * $sinrot) + $xp]
+      set y3p [expr (-$x3 * $sinrot + $y3 * $cosrot) + $yp]
+
+      set x4p [expr ($x4 * $cosrot + $y4 * $sinrot) + $xp]
+      set y4p [expr (-$x4 * $sinrot + $y4 * $cosrot) + $yp]
+
+# Project these points into the focal plane
+
+      set x1p [expr ($x1p - $xc)/$pwfsScale + $xc ]
+      set y1p [expr ($y1p - $yc)/$pwfsScale + $yc ]
+
+      set x2p [expr ($x2p - $xc)/$pwfsScale + $xc ]
+      set y2p [expr ($y2p - $yc)/$pwfsScale + $yc ]
+
+      set x3p [expr ($x3p - $xc)/$pwfsScale + $xc ]
+      set y3p [expr ($y3p - $yc)/$pwfsScale + $yc ]
+
+      set x4p [expr ($x4p - $xc)/$pwfsScale + $xc ]
+      set y4p [expr ($y4p - $yc)/$pwfsScale + $yc ]
+
+# Draw a polygon connecting these points
+
+      if {$vigType == "partial" } {
+        $canvas create polygon $x1p $y1p $x2p $y2p $x3p $y3p $x4p $y4p \
+          -fill $pwfsColor -stipple gray12  \
+          -tags objects
+      } elseif {$vigType == "blocked"} {
+        $canvas create polygon $x1p $y1p $x2p $y2p $x3p $y3p $x4p $y4p \
+          -fill $pwfsColor -stipple gray50 \
+          -tags objects
+      } else {
+        $canvas create polygon $x1p $y1p $x2p $y2p $x3p $y3p $x4p $y4p \
+          -outline $pwfsColor -fill "" -tags objects
+      }
+  }
+
+  proc toggle_arm {pwfs} {
+
+   global togglePwfs1 togglePwfs2
+   global canvas
+
+   if {$pwfs == "pwfs1"} {
+     if {$togglePwfs1 == "add"} {
+       set togglePwfs1 subtract
+     } else {
+       set togglePwfs1 add
+     }
+   } else {
+     if {$togglePwfs2 == "add"} {
+       set togglePwfs2 subtract
+     } else {
+       set togglePwfs2 add
+     }
+   }
+  
+   $canvas delete objects
+   draw_field
+
+  }
 }
