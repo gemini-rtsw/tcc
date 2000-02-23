dcb5ade5de77654579386e13b54eca428c072b1c
dlt <dlt@gemini.edu>
2000-02-23 01:55:35 +0000

    Implement target copy
    git-svn-id: http://source.gemini.edu/software/tcc/trunk@8887 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- tcclib/createObjects.tcl
+++ tcclib/createObjects.tcl
@@ -6,105 +6,68 @@
 #  tcl applications. If an object of the same name and type already exists 
 #  it is deleted.
 #
-#  D Terrett 21 February 2000
+#  D Terrett 22 February 2000
 #
 #  Copyright CCLRC
 #-
 
 proc createConfig {name args} {
-   if { [llength [itcl::find  objects ::TcsConfigNames::$name \
-         -class ConfigComponent]] != 0} {
-      delete object ::TcsConfigNames::$name
-   }
+   catch {delete object ::TcsConfigNames::$name}
    eval ConfigComponent \"::TcsConfigNames::$name\" TcsConfigList $args
 }
 
 proc createScienceTarget {name args} {
-   if { [llength [itcl::find  objects ::ScienceTargetNames::$name \
-         -class SciTargetComponent]] != 0} {
-      delete object ::ScienceTargetNames::$name
-   }
+   catch {delete object ::ScienceTargetNames::$name}
    eval SciTargetComponent \"::ScienceTargetNames::$name\" ScienceTargetList $args
 }
 
 proc createWfsTarget {name args} {
-   if { [llength [itcl::find  objects ::WfsTargetNames::$name \
-         -class TargetComponent]] != 0} {
-      delete object ::WfsTargetNames::$name
-   }
-   eval TargetComponent \"::WfsTargetNames::$name\" WfsTargetList $args
+   catch {delete object ::WfsTargetNames::$name}
+   eval WfsTargetComponent \"::WfsTargetNames::$name\" WfsTargetList $args
 }
 
 proc createPointingOrigin {name args} {
-   if { [llength [itcl::find  objects ::PointOrigNames::$name \
-         -class PointOrigComponent]] != 0} {
-      delete object ::PointOrigNames::$name
-   }
+   catch {delete object ::PointOrigNames::$name}
    eval PointOrigComponent \"::PointOrigNames::$name\" PointOrigList $args
 }
 
 proc createSlewOptions {name args} {
-   if { [llength [itcl::find  objects ::SlewOptionsNames::$name \
-         -class SlewOptionsComponent]] != 0} {
-      delete object ::SlewOptionsNames::$name
-   }
+   catch {delete object ::SlewOptionsNames::$name}
    eval SlewOptionsComponent \"::SlewOptionsNames::$name\" SlewOptionsList $args
 }
 
 proc createTrackFrame {name args} {
-   if { [llength [itcl::find  objects ::TrackFrameNames::$name \
-         -class TrackFrameComponent]] != 0} {
-      delete object ::TrackFrameNames::$name
-   }
+   catch {delete object ::TrackFrameNames::$name}
    eval TrackFrameComponent \"::TrackFrameNames::$name\" TrackFrameList $args
 }
 
 proc createWavelength {name args} {
-   if { [llength [itcl::find  objects ::WavelengthNames::$name \
-         -class WavelengthComponent]] != 0} {
-      delete object ::WavelengthNames::$name
-   }
+   catch {delete object ::WavelengthNames::$name}
    eval WavelengthComponent \"::WavelengthNames::$name\" WavelengthList $args
 }
 
 proc createInstrument {name args} {
-   if { [llength [itcl::find  objects ::InstrumentNames::$name \
-         -class InstrumentComponent]] != 0} {
-      delete object ::InstrumentNames::$name
-   }
+   catch {delete object ::InstrumentNames::$name}
    eval InstrumentComponent \"::InstrumentNames::$name\" InstrumentList $args
 }
 
 proc createTrackRate {name args} {
-   if { [llength [itcl::find  objects ::TrackRateNames::$name \
-         -class TrackRateComponent]] != 0} {
-      delete object ::TrackRateNames::$name
-   }
+   catch {delete object ::TrackRateNames::$name}
    eval TrackRateComponent \"::TrackRateNames::$name\" TrackRateList $args
 }
 
 proc createRotator {name args} {
-   if { [llength [itcl::find  objects ::RotatorNames::$name \
-            -class RotatorComponent]] != 0} {
-      delete object ::RotatorNames::$name
-   }
+   catch {delete object ::RotatorNames::$name}
    eval RotatorComponent \"::RotatorNames::$name\" RotatorList $args
 }
 
 proc createGuide {name args} {
-   if { [llength [itcl::find  objects ::GuideNames::$name \
-         -class GuideComponent]] != 0} {
-      delete object ::GuideNames::$name
-   }
+   catch {delete object ::GuideNames::$name}
    eval GuideComponent \"::GuideNames::$name\" GuideList $args
 }
 
-
 proc createCosys {name args} {
-   if { [llength [itcl::find  objects ::CosysNames::$name \
-         -class CosysComponent]] != 0} {
-      delete object ::CosysNames::$name
-   }
+   catch {delete object ::CosysNames::$name}
    eval CosysComponent \"::CosysNames::$name\" CosysList $args
 }
 
