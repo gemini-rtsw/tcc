aa55d1223371a98c1af9327d92cb1f6747b1708d
dlt <dlt@gemini.edu>
2001-01-29 13:03:43 +0000

    Bitmaps for new handsets
    git-svn-id: http://source.gemini.edu/software/tcc/trunk@9128 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- src/bitmaps.c
+++ src/bitmaps.c
@@ -7,7 +7,7 @@ static char rcsid[]="$Id:";
 *   FUNCTION NAME(S)
 *   Bitmaps_Init - Initialisation function for TCL loadable image
 *
-*   D L Terrett 20 September 2000
+*   D L Terrett 10 January 2001
 *
 *   Copyright CCLRC
 *
@@ -39,6 +39,10 @@ int Bitmaps_Init( Tcl_Interp *interp)
 
 #include "edit.xbm"
 #include "delete.xbm"
+#include "rotate.xbm"
+#include "arotate.xbm"
+#include "flip.xbm"
+#include "reset.xbm"
 
     if ( Tcl_InitStubs( interp, "8.0", 0 ) == NULL ) return TCL_ERROR;
 
@@ -49,6 +53,26 @@ int Bitmaps_Init( Tcl_Interp *interp)
 
     if ( Tk_DefineBitmap( interp, Tk_GetUid("delete"), (char*)delete_bits, 
         delete_width, delete_height) != TCL_OK ) {
+        return TCL_ERROR;
+    }
+
+    if ( Tk_DefineBitmap( interp, Tk_GetUid("rotate"), (char*)rotate_bits, 
+        rotate_width, rotate_height) != TCL_OK ) {
+        return TCL_ERROR;
+    }
+
+    if ( Tk_DefineBitmap( interp, Tk_GetUid("arotate"), (char*)arotate_bits, 
+        arotate_width, arotate_height) != TCL_OK ) {
+        return TCL_ERROR;
+    }
+
+    if ( Tk_DefineBitmap( interp, Tk_GetUid("flip"), (char*)flip_bits, 
+        flip_width, flip_height) != TCL_OK ) {
+        return TCL_ERROR;
+    }
+
+    if ( Tk_DefineBitmap( interp, Tk_GetUid("reset"), (char*)reset_bits, 
+        reset_width, reset_height) != TCL_OK ) {
         return TCL_ERROR;
     }
 
