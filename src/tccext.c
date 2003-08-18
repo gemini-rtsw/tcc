d1b3f28f24b9dd10ebee9a070437294312003b25
dlt <dlt@gemini.edu>
2003-08-18 09:24:36 +0000

    Move bitmaps to script code
    git-svn-id: http://source.gemini.edu/software/tcc/trunk@9932 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- src/tccext.c
+++ src/tccext.c
@@ -7,7 +7,7 @@ static char rcsid[]="$Id:";
 *   FUNCTION NAME(S)
 *   Tccext_Init - Initialisation function for TCL loadable image
 *
-*   D L Terrett 7 June 2002
+*   D L Terrett 18 August 2003
 *
 *   Copyright CCLRC
 *
@@ -22,13 +22,6 @@ static char rcsid[]="$Id:";
 #include "tccAst.h"
 #include "autorepeat.h"
 #include "plosc.h"
-#include "edit.xbm"
-#include "delete.xbm"
-#include "rotate.xbm"
-#include "arotate.xbm"
-#include "hflip.xbm"
-#include "vflip.xbm"
-#include "reset.xbm"
 
 /* *INDENT-OFF* */
 /*+
@@ -60,41 +53,6 @@ int Tccext_Init( Tcl_Interp *interp)
 
     Tcl_CreateObjCommand( interp, "plosc", ploscCmd,
         (ClientData) NULL, (Tcl_CmdDeleteProc *) NULL);
-
-    if ( Tk_DefineBitmap( interp, Tk_GetUid("edit"), (char*)edit_bits,
-        edit_width, edit_height) != TCL_OK ) {
-        return TCL_ERROR;
-    }
-
-    if ( Tk_DefineBitmap( interp, Tk_GetUid("delete"), (char*)delete_bits,
-        delete_width, delete_height) != TCL_OK ) {
-        return TCL_ERROR;
-    }
-
-    if ( Tk_DefineBitmap( interp, Tk_GetUid("rotate"), (char*)rotate_bits,
-        rotate_width, rotate_height) != TCL_OK ) {
-        return TCL_ERROR;
-    }
-
-    if ( Tk_DefineBitmap( interp, Tk_GetUid("arotate"), (char*)arotate_bits,
-        arotate_width, arotate_height) != TCL_OK ) {
-        return TCL_ERROR;
-    }
-
-    if ( Tk_DefineBitmap( interp, Tk_GetUid("hflip"), (char*)hflip_bits,
-        hflip_width, hflip_height) != TCL_OK ) {
-        return TCL_ERROR;
-    }
-
-    if ( Tk_DefineBitmap( interp, Tk_GetUid("vflip"), (char*)vflip_bits,
-        vflip_width, vflip_height) != TCL_OK ) {
-        return TCL_ERROR;
-    }
-
-    if ( Tk_DefineBitmap( interp, Tk_GetUid("reset"), (char*)reset_bits,
-        reset_width, reset_height) != TCL_OK ) {
-        return TCL_ERROR;
-    }
 
     return TCL_OK;
 }
