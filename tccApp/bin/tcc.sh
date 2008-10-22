8265fbd12c06d360660589044e7591a5836b0aea
fkraemer <fkraemer@gemini.edu>
2008-10-22 00:31:35 +0000

    renamed TCC_XML_PATH to TCC_INITPATH
    git-svn-id: http://source.gemini.edu/software/tcc/trunk@14686 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- tccApp/bin/tcc.sh
+++ tccApp/bin/tcc.sh
@@ -4,13 +4,13 @@ cd ${GEMINI_TOP}/opt/tcc
 
 for i in $*; do
         case $1 in
-		-init) shift; export TCC_XML_PATH=$1; shift;;
+		-init) shift; export TCC_INITPATH=$1; shift;;
 		*) ARGS="$ARGS $1"; shift;;
 	esac
 done 
-if (( !$TCC_XML_PATH )); then
-	export TCC_XML_PATH=${GEMINI_TOP}/opt/tcc/var/local-config/${GEMINI_SITE}
+if [ "$TCC_XML_PATH" = "" ]; then
+	export TCC_INITPATH=${GEMINI_TOP}/opt/tcc/var/local-config/${GEMINI_SITE}
 fi
-echo "Using TCC_XML_PATH=$TCC_XML_PATH"
+echo "Using TCC_INITPATH=$TCC_INITPATH"
 echo "ARGS: $ARGS"
-./tcc $ARGS
+./tcc $ARGS -init $TCC_INITPATH
