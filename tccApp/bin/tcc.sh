96acd584fafcd507f0b65d9576a0147ee8cf3a1c
fkraemer <fkraemer@gemini.edu>
2008-10-22 00:48:19 +0000

    changed order of some lines in tcc.sh script so that -init option overrides all other possibilities to set TCC_INITPATH
    git-svn-id: http://source.gemini.edu/software/tcc/trunk@14692 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- tccApp/bin/tcc.sh
+++ tccApp/bin/tcc.sh
@@ -2,15 +2,16 @@
 
 cd ${GEMINI_TOP}/opt/tcc
 
+if [ "$TCC_XML_PATH" = "" ]; then
+	export TCC_INITPATH=${GEMINI_TOP}/opt/tcc/var/local-config/${GEMINI_SITE}
+fi
 for i in $*; do
         case $1 in
 		-init) shift; export TCC_INITPATH=$1; shift;;
 		*) ARGS="$ARGS $1"; shift;;
 	esac
 done 
-if [ "$TCC_XML_PATH" = "" ]; then
-	export TCC_INITPATH=${GEMINI_TOP}/opt/tcc/var/local-config/${GEMINI_SITE}
-fi
+
 echo "Using TCC_INITPATH=$TCC_INITPATH"
 echo "ARGS: $ARGS"
 ./tcc $ARGS -init $TCC_INITPATH
